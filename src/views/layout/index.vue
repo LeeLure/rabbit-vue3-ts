ef61661b45b8f0c361746370561ed9d5733b6ba2
lijiawei <747518667@qq.com>
2022-07-05 16:03:54 +0800

    feat: 顶部通栏

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup name="Layout">
+import AppTopnav from './components/app-topnav.vue';
 // yarn add vite-plugin-vue-setup-extend -D 为了给组件取名字，但是如果 script 里面没有任何内容，
 // 在渲染是会被丢掉，故此文字只为让组件名在浏览器显示
 </script>
 <template>
-  <div>layout组件</div>
+  <div>
+    <AppTopnav />
+  </div>
 </template>
 
 <style lang="less" scoped>
