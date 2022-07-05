ad38643581aa205de8653031fcde07ecfbd0b18b
lijiawei <747518667@qq.com>
2022-07-05 16:37:39 +0800

    feat: 抽封头部导航的分类、 favicon 、及底部布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup name="Layout">
 import AppTopnav from './components/app-topnav.vue';
+import AppHeader from './components/app-header.vue';
+import AppFooter from './components/app-footer.vue';
 // yarn add vite-plugin-vue-setup-extend -D 为了给组件取名字，但是如果 script 里面没有任何内容，
 // 在渲染是会被丢掉，故此文字只为让组件名在浏览器显示
 </script>
 <template>
   <div>
     <AppTopnav />
+    <AppHeader />
+    <div style="height: 1200px;">二级路由出口 router-view </div>
+    <AppFooter />
   </div>
 </template>
 
