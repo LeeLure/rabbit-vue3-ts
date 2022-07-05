64379373af130ee74389bfae278eee9095d68e40
lijiawei <747518667@qq.com>
2022-07-05 18:18:19 +0800

    feat: 渲染二级分类

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -2,6 +2,8 @@ export interface CategoryItem {
   id: string
   name: string
   picture: string
+  // 巧妙的设计：这是类型，不会造成死递归栈溢出
+  children: CategoryItem[]
 }
 
 export interface ApiRes<T> {
--- src/views/layout/components/app-header-nav.vue
+++ src/views/layout/components/app-header-nav.vue
@@ -7,11 +7,10 @@
       <RouterLink to="/">{{ item.name }}</RouterLink>
       <div class="layer">
         <ul>
-          <li v-for="i in 10" :key="i">
+          <li v-for="sub in item.children" :key="sub.id">
             <a href="#">
-              <img src="https://yanxuan.nosdn.127.net/cc361cf40d4f81c7eccefed1ad18face.png?quality=95&imageView"
-                alt="" />
-              <p>果干</p>
+              <img :src="sub.picture" alt="" />
+              <p>{{ sub.name }}</p>
             </a>
           </li>
         </ul>
