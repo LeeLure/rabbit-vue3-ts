253142329020172179b3da79c601b4ec77012430
lijiawei <747518667@qq.com>
2022-07-06 14:30:56 +0800

    feat: 头部吸顶导航

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,6 +2,7 @@
 import AppTopnav from './components/app-topnav.vue';
 import AppHeader from './components/app-header.vue';
 import AppFooter from './components/app-footer.vue';
+import AppHeaderSticky from './components/app-header-sticky.vue';
 // yarn add vite-plugin-vue-setup-extend -D 为了给组件取名字，但是如果 script 里面没有任何内容，
 // 在渲染是会被丢掉，故此文字只为让组件名在浏览器显示
 </script>
@@ -9,6 +10,8 @@ import AppFooter from './components/app-footer.vue';
   <div>
     <AppTopnav />
     <AppHeader />
+    <!-- 吸顶导航 -->
+    <AppHeaderSticky />
     <div class="app-body" style="height: 1200px;">
       <!-- 二级路由出口 -->
       <RouterView />
