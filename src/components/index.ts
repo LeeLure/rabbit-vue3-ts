ce935e943cdd3150a22dddca3f529424834a9e0c
lijiawei <747518667@qq.com>
2022-07-08 15:57:21 +0800

    feat: 封装查看全部组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import { App } from 'vue'
 import XtxSkeleton from './skeleton/index.vue'
 import XtxCarousel from './carousel/index.vue'
+import XtxMore from './more/index.vue'
 
 
 export default {
@@ -8,5 +9,6 @@ export default {
     // 全局注册组件
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,13 @@
 import XtxSkeleton from '@/components/skeleton/index.vue'
 import XtxCarousel from '@/components/carousel/index.vue'
+import XtxMore from '@/components/more/index.vue'
 // 参考：
 declare module 'vue' {
   export interface GlobalComponents {
     // 组件名：typeof 组件对象
     XtxSkeleton: typeof XtxSkeleton
     XtxCarousel: typeof XtxCarousel
+    XtxMore: typeof XtxMore
   }
 }
 export { }
