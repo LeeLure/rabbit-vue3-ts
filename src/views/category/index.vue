7e0382863e4edfea225824540fd1bd79f574251e
lijiawei <747518667@qq.com>
2022-07-11 10:13:53 +0800

    feat: 渲染轮播图

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -3,7 +3,7 @@ import useStore from '@/store';
 import { watch, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
-const { category } = useStore()
+const { category, home } = useStore()
 
 // 获取路由参数
 const route = useRoute()
@@ -28,16 +28,28 @@ watchEffect(() => {
   // }
 })
 
+// 应该获取分类下的轮播图数据，但是后台没有提供接口
+// category.getgetBannerList()
+// 故使用首页的数据替代
+home.getBannerList()
+
 </script>
 <template>
-  <div class="category">分类组件
+  <div class="category">
     <!-- provide 和 inject 的作用是注入数据, 例如在 bread 上注入的数据, 所有的子子孙孙组件都可以通过 inject 访问 -->
     <!-- 以下嵌套关系也支持通过 inject 访问 -->
     <!-- 面包屑 -->
-    <XtxBread>
-      <XtxBreadItem>首页</XtxBreadItem>
-      <XtxBreadItem>{{ category.topCategoryList.name }}</XtxBreadItem>
-    </XtxBread>
+    <div class="carousel">
+      <XtxBread>
+        <XtxBreadItem>首页</XtxBreadItem>
+        <XtxBreadItem>{{ category.topCategoryList.name }}</XtxBreadItem>
+      </XtxBread>
+
+      <!-- 轮播图组件 -->
+      <XtxCarousel style="height: 500px;" :slides="home.bannerList" />
+
+
+    </div>
   </div>
 </template>
 
