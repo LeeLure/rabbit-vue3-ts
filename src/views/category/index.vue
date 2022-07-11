39ab08ac3a8194e635fb052e136450011960fb70
lijiawei <747518667@qq.com>
2022-07-11 11:22:33 +0800

    feat: 封装商品展示并渲染

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -2,6 +2,7 @@
 import useStore from '@/store';
 import { watch, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
+import GoodsItem from './components/goods-item.vue';
 
 const { category, home } = useStore()
 
@@ -39,7 +40,7 @@ home.getBannerList()
     <!-- provide 和 inject 的作用是注入数据, 例如在 bread 上注入的数据, 所有的子子孙孙组件都可以通过 inject 访问 -->
     <!-- 以下嵌套关系也支持通过 inject 访问 -->
     <!-- 面包屑 -->
-    <div class="carousel">
+    <div class="container">
       <XtxBread>
         <XtxBreadItem>首页</XtxBreadItem>
         <XtxBreadItem>{{ category.topCategoryList.name }}</XtxBreadItem>
@@ -61,6 +62,17 @@ home.getBannerList()
         </ul>
       </div>
 
+      <!-- 分类关联商品 -->
+      <div class="ref-goods" v-for="item in category.topCategoryList.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }} -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem :goods="goods" v-for="goods in item.goods" :key="goods.id" />
+        </div>
+      </div>
 
     </div>
   </div>
@@ -77,6 +89,37 @@ home.getBannerList()
     line-height: 100px;
   }
 
+  // 分类关联商品
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
+    }
+  }
+
+  // 二级分类
   .sub-list {
     margin-top: 20px;
     background-color: #fff;
