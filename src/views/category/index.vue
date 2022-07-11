37febc2591a4d7e7d353d7faab2b985eac19adb1
lijiawei <747518667@qq.com>
2022-07-11 10:28:14 +0800

    feat: 渲染二级分类

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -35,7 +35,7 @@ home.getBannerList()
 
 </script>
 <template>
-  <div class="category">
+  <div class="top-category">
     <!-- provide 和 inject 的作用是注入数据, 例如在 bread 上注入的数据, 所有的子子孙孙组件都可以通过 inject 访问 -->
     <!-- 以下嵌套关系也支持通过 inject 访问 -->
     <!-- 面包屑 -->
@@ -48,11 +48,68 @@ home.getBannerList()
       <!-- 轮播图组件 -->
       <XtxCarousel style="height: 500px;" :slides="home.bannerList" />
 
+      <!-- 所有二级分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="item in category.topCategoryList.children" :key="item.id">
+            <a href="javascript:;">
+              <img v-slowimage="item.picture">
+              <p>{{ item.name }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+
 
     </div>
   </div>
 </template>
 
 
-<style>
+<style scoped lang="less">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
