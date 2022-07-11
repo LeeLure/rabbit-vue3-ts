3e3c0fc2679c722733741e1538916bb7ea575a4b
lijiawei <747518667@qq.com>
2022-07-11 17:42:25 +0800

    feat: 商品详情-放大镜基本结构

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -10,6 +10,8 @@ const active = ref(0)
   <div class="goods-image">
     <div class="middle">
       <img :src="images[active]" alt="" />
+      <div class="large" :style="[{ backgroundImage: `url(${images[active]})` }]"></div>
+      <div class="layer"></div>
     </div>
     <ul class="small">
       <li @mouseenter="active = index" v-for="(item, index) in images" :key="item"
@@ -26,11 +28,35 @@ const active = ref(0)
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
 
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    position: relative;
+    cursor: move;
+
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, .2);
+      left: 0;
+      top: 0;
+      position: absolute
+    }
   }
 
   .small {
