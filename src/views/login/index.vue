9fe7baa48d4c6aed20ee106d45c766b9babf18bd
lijiawei <747518667@qq.com>
2022-07-15 10:18:37 +0800

    feat: 登录功能-切换方式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup name="Login">
 import LoginHeader from './components/login-header.vue'
 import LoginFooter from './components/login-footer.vue'
+import { ref } from 'vue';
+
+// 字面量类型
+const active = ref<'account' | 'qrcode'>('account')
 </script>
 <template>
   <LoginHeader>欢迎登录</LoginHeader>
   <section class="login-section">
     <div class="wrapper">
       <nav>
-        <a href="javascript:;">账户登录</a>
-        <a href="javascript:;">扫码登录</a>
+        <a :class="{ active: active === 'account' }" @click="active = 'account'" href="javascript:;">账户登录</a>
+        <a :class="{ active: active === 'qrcode' }" @click="active = 'qrcode'" href="javascript:;">扫码登录</a>
       </nav>
+
+      <!-- 表单 -->
+      <div v-if="active === 'account'" class="account-box">表单</div>
+      <!-- 二维码 -->
+      <div v-else class="qrcode-box">
+        <img src="@/assets/images/qrcode.jpg" alt="" />
+        <p>打开 <a href="javascript:;">小兔鲜App</a> 扫码登录</p>
+      </div>
     </div>
   </section>
   <LoginFooter />
@@ -30,6 +42,21 @@ import LoginFooter from './components/login-footer.vue'
     top: 54px;
     transform: translate3d(100px, 0, 0);
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+
+    // 二维码容器
+    .qrcode-box {
+      text-align: center;
+      padding-top: 40px;
+
+      p {
+        margin-top: 20px;
+
+        a {
+          color: @xtxColor;
+          font-size: 16px;
+        }
+      }
+    }
 
     nav {
       height: 55px;
