63c443b9c4aa058a5374e354877278e82bbd8e51
lijiawei <747518667@qq.com>
2022-07-15 09:29:12 +0800

    feat: 登录功能-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,62 @@
 <script lang="ts" setup name="Login">
-// yarn add vite-plugin-vue-setup-extend -D 为了给组件取名字，但是如果 script 里面没有任何内容，
-// 在渲染是会被丢掉，故此文字只为让组件名在浏览器显示
+import LoginHeader from './components/login-header.vue'
+import LoginFooter from './components/login-footer.vue'
 </script>
 <template>
-  <div>login组件</div>
+  <LoginHeader>欢迎登录</LoginHeader>
+  <section class="login-section">
+    <div class="wrapper">
+      <nav>
+        <a href="javascript:;">账户登录</a>
+        <a href="javascript:;">扫码登录</a>
+      </nav>
+    </div>
+  </section>
+  <LoginFooter />
 </template>
 
-<style lang="less" scoped>
+<style scoped lang="less">
+.login-section {
+  background: url(@/assets/images/login-bg.png) no-repeat center / cover;
+  height: 488px;
+  position: relative;
+
+  .wrapper {
+    width: 380px;
+    background: #fff;
+    min-height: 400px;
+    position: absolute;
+    left: 50%;
+    top: 54px;
+    transform: translate3d(100px, 0, 0);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+
+    nav {
+      height: 55px;
+      border-bottom: 1px solid #f5f5f5;
+      display: flex;
+      padding: 0 40px;
+      text-align: right;
+      align-items: center;
+
+      a {
+        flex: 1;
+        line-height: 1;
+        display: inline-block;
+        font-size: 18px;
+        position: relative;
+
+        &:first-child {
+          border-right: 1px solid #f5f5f5;
+          text-align: left;
+        }
+
+        &.active {
+          color: @xtxColor;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+}
 </style>
